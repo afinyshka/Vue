47d5813094ebfbbd505cedaecf0adb9589ad6de3
afinyshka <afinyshka@mail.ru>
2023-08-23 00:08:05 +0300

    add hw05

---
+++ HWork/HW05/product-details/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <ProductDetails :product="product1" />
+  <ProductDetails :product="product2" />
+</template>
+
+<script>
+import ProductDetails from './components/ProductDetails.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ProductDetails
+  },
+  data() {
+    return {
+      product1: { name: 'Tropical Island', price: 78, available: false },
+      product2: { name: 'Hot Safary', price: 95, available: true }
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+#app
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  margin-top: 60px
+.out-of-stock
+  color: red
+</style>
---
+++ HWork/HW05/product-details/src/components/ProductDetails.vue
@@ -0,0 +1,38 @@
+<template>
+  <h2>{{ product.name }}</h2>
+  <p>{{ formattedPrice }}</p>
+  <p :class="{ 'out-of-stock': !product.available, 'in-stock': product.available }"> {{ availabilityStatus }} </p>
+</template>
+
+<script>
+export default {
+  name: 'ProductDetails',
+  props: {
+    product: {
+      name: String,
+      price: Number,
+      available: Boolean,
+    }
+  },
+  computed: {
+    formattedPrice() {
+      return `$${this.product.price}`
+    },
+    availabilityStatus() {
+      return this.product.available ? 'Available' : 'Out of stock'
+    }
+  },
+  methods: {
+
+  },
+}
+</script>
+
+
+<style scoped lang = "sass">
+.out-of-stock
+  color: red
+
+.in-stock
+  color: green
+</style>
